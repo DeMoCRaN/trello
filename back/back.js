0b43049f103fbd570711356afd6757f311df720c
DeMoCRaN <giffg522@gmail.com>
2025-07-03 20:10:47 +0300

    4 commit for backend

--- back/back.js
+++ back/back.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const statusPageHandler = require('./def/statusPage');
+
+app.use(express.json());
+
+// PostgreSQL connection pool setup
+const pool = new Pool({
+  user: 'democran',
+  host: 'localhost', 
+  database: 'postgres', 
+  password: 'qweasd-123',
+  port: 5433, 
 });
+// Root route
+app.get('/', statusPageHandler);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
